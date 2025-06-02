ded4045e2b02f8ea70993d8f2d5c88dcb963b1a2
rajgharat07 <gharatyashraj@gmail.com>
2025-06-02 17:07:42 +0530

    backend url updated

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 //const BASE_URL = "https://calculator.adaptable.app";
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://plant-diseases-zj7a.onrender.com"
 const api = axios.create({
   baseURL: BASE_URL,
 });
